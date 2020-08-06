e89cae50f1ead49e4158128769edf77fdcc00492
srikant.viswanath <srikant.viswanath@invitae.com>
2020-08-06 16:00:28 -0400

    tweaks another_project_settings.test_settings test to include SETTINGS_ENV in assertion

--- another_project_with_nested_settings/test_settings.py
+++ another_project_with_nested_settings/test_settings.py
@@ -6,6 +6,12 @@ class TestNestedSettingsProject(unittest.TestCase):
     def test_simple_settings_get(self):
         with another_project_settings('prd'):
             self.assertEqual(another_project_settings.priority_level, 'prd_priority_level')
+            self.assertEqual(another_project_settings.SETTINGS_ENV, 'prd')
+        with another_project_settings('dev'):
+            self.assertEqual(another_project_settings.priority_level, 'dev_priority_level')
+            self.assertEqual(another_project_settings.SETTINGS_ENV, 'dev')
+        with self.assertRaises(EnvironmentError):
+            print(another_project_settings.priority_level)
 
 
 if __name__ == '__main__':
